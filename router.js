3a9a5b974cabb6fbc21e0b85a868b301504f4145
echeverriaalex <aleex.naahuel@outlook.com>
2022-12-21 21:46:37 -0300

    add routes and html

--- router.js
+++ router.js
@@ -1,8 +1,41 @@
+const { render } = require('ejs');
 const express = require('express');
+const app = express();
 const router = express.Router();
 
 const characterController = require('./controllers/CharactersController');
 
+router.get('/home', (req, res, next)=>{
+    res.send(`Welcome to Disney Api`);
+})
+
+router.get('/developer', (req, res, next)=>{
+    res.json({
+        name : "Alex",
+        secondName: "Nahuel",
+        lastname: "Echeverria",
+        rol: "Backend",
+        skills: "NodeJS, JS, PHP, HTML5, Java, MySQL, PostgreSQL"
+    });
+})
+
 router.get('/characters', characterController.getCharacters);
+
+/* 
+// no se puede hacer porque getcharacters lanza una respuesta
+router.get('/characters', (req, res, next)=>{
+    
+    data = characterController.getCharacters();
+    console.log(data);
+    res.send('showed')
+
+});
+*/
+
+router.get('/character/:name', characterController.getCharacterByName);
+router.get('/character/:id', characterController.getCharacterById);
+router.get('/charactersbasicinfo', characterController.getBasicInfoCharacters);
+
+router.use(express.static('public'));
 
 module.exports = router;
