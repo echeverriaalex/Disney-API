0a4626ee31e50cdbaac2eb6145eca683c5c4d72d
echeverriaalex <aleex.naahuel@outlook.com>
2022-12-21 21:46:15 -0300

    add settings

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const router = require('./router');
 const app = express();
 const port = 3000;
 
+// Settings
+app.set('developer', 'Alex Nahuel Echeverria')
+app.set('view engine', 'ejs');
+
 app.use('/', router);
 
 app.listen(port, ()=>{
 
     console.log(`Developing Disney API on port ${port}`);
+    //console.log('developed by' + app.get(developer));
 })
